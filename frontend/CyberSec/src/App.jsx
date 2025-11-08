401e48640190ef04b950b734ff5de110acfda07b
kaisalayasa <kaisalayasa@gmail.com>
2025-11-07 23:54:24 -0500

    feat: intial commit

---
+++ frontend/CyberSec/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MainGamePage from './pages/MainGamePage'
+
+function App() {
+ 
+
+  return (
+    <>
+    <MainGamePage/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ frontend/CyberSec/src/pages/MainGamePage.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import styles from './MainGamePage.module.css'
+
+function MainGamePage() {
+  return (
+    <>
+      <div className={styles.layout}>
+        <div className={styles.header}>
+          <h1>CyberSmart: Scam Awareness Game</h1>
+          <p>Learn how to spot scams and stay safe online</p>
+        </div>
+
+        <section className={styles.project_container}>
+          {/* Your game will go here */}
+        </section>
+
+        <section className={styles.overview}>
+          <h1>Overview</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda minima quam ipsa ipsam aut sint perferendis illo placeat ducimus, natus consequuntur sunt fuga dolorem doloribus cumque quos nihil reprehenderit veritatis.
+          </p>
+        </section>
+
+        <section className={styles.key_learning}>
+          <h1>Key Learnings</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi asperiores repellendus, sunt vero nostrum quae eos consectetur. Modi dignissimos officiis mollitia quidem ratione? Architecto, sapiente? Beatae quidem quae perferendis quia.
+          </p>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default MainGamePage
